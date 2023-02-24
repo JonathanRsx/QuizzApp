7907b2e5f76c20b11f035f933f342acc1e49f47c
JonathanRsx <jonathan@akoonetcom>
2023-02-24 16:50:28 +0100

    Updates

---
+++ precache-manifest.59ca29668d02f51b4777cd495db8803c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb1c083f34fc8178e04c",
+    "url": "/QuizzApp/static/css/main.b9233ce6.chunk.css"
+  },
+  {
+    "revision": "bb1c083f34fc8178e04c",
+    "url": "/QuizzApp/static/js/main.bb1c083f.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/QuizzApp/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "05ff5739ada61bb94006",
+    "url": "/QuizzApp/static/js/2.05ff5739.chunk.js"
+  },
+  {
+    "revision": "d57cfdc943be8723d2d2",
+    "url": "/QuizzApp/static/js/runtime~main.d57cfdc9.js"
+  },
+  {
+    "revision": "b21bb1e343c5f23481e82d3a51e08e52",
+    "url": "/QuizzApp/static/media/Right.b21bb1e3.svg"
+  },
+  {
+    "revision": "22253a65076a36ace29f378b59588e0f",
+    "url": "/QuizzApp/static/media/Wrong.22253a65.svg"
+  },
+  {
+    "revision": "03b6cb59906159e3a631625957ff0543",
+    "url": "/QuizzApp/index.html"
+  }
+];
